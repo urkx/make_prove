987dac7b482c3740f8ba348c798050cbb1bb46aa
Urko López <urkolopezlopez@gmail.com>
2021-02-20 23:13:04 +0100

    Image generator method

--- lib.h
+++ lib.h
@@ -1,3 +1,6 @@
+#include <iostream>
+#include <fstream>
+
 template <typename T>
 struct List{
 	T primero;
@@ -23,4 +26,27 @@ int Gen::getId(){
 
 void Gen::setId(int i){
 	id = i;
+}
+
+
+void createImage(int width, int height)
+{
+	std::ofstream out ("./scene.ppm", std::ios::out);
+	out << "P3\n" << width << " " << height << "\n255\n";
+
+	 for (int j = height-1; j >= 0; --j) {
+	 	for (int i = 0; i < width; ++i) {
+			auto r = double(i) / (width-1);
+			auto g = double(j) / (height-1);
+			auto b = 0.25;
+			
+			int ir = static_cast<int>(255.999 * r);
+			int ig = static_cast<int>(255.999 * g);
+			int ib = static_cast<int>(255.999 * b);
+
+			out << ir << ' ' << ig << ' ' << ib << '\n';
+		}
+	}
+
+	out.close();
 }
--- main.cpp
+++ main.cpp
@@ -1,19 +1,10 @@
 #include <iostream>
 #include "lib.h"
 
+
+
 int main(){
-	struct List <double> l;
-	Gen g(1);
-	l.primero = 1.05;
-	l.segundo = 2.05;
 	
-	std::cout << "Primer valor: " << l.primero << std::endl;
-	std::cout << "Segundo valor: " << l.segundo << std::endl;
-	
-	g.setId(3);
-
-	std::cout << "Id: " << g.getId() << std::endl;
-
+	createImage(256, 256);
 	return 0;
-
 }
